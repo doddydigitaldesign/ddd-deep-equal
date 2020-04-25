19e86315614ef2cdb41ef15a62a22ac8e8b1826f
Daniel Sonne Lehnberg <doddy@doddy.se>
2020-04-25 18:44:20 +0200

    add webpack, update configs

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src"),
+
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+
+  module: {
+    rules: [
+      { test: /\.(ts|js)x?$/, loader: "babel-loader", exclude: /node_modules/ },
+    ],
+  },
+};
